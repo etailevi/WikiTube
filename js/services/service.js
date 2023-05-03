d6bd8d5e05f6c21d97422f952617212aad5ed6d7
Etai Levi <etai3000@gmail.com>
2023-05-03 15:57:55 +0300

    API KEY was deleted from project for safety

--- js/services/service.js
+++ js/services/service.js
@@ -1,14 +1,12 @@
 'use strict'
 
-const API_KEY = 'AIzaSyC4AhuuH0mF9Qr2NxI7pkJSkmzqgefxY98'
-const API_KEY2 = 'AIzaSyD-7JXS15AWR6Kt29jCftH2Vpc0Bszh2ig'
-const API_KEY3 = 'AIzaSyA13ULgHZv3MrvSQoFX_MR3C2-5ALY-wgQ'
+const API_KEY = ''
 const youtubeDB = 'youtubeDB'
 const gYouTubeCache = loadFromStorage(youtubeDB) || {}
 
 function askVideo(value) {
     if (gYouTubeCache[value]) return Promise.resolve(gYouTubeCache[value])
-    return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${API_KEY3}&q=${value}`)
+    return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${API_KEY}&q=${value}`)
         .then(res => { // resolve
             gYouTubeCache[value] = res.data.items
             saveToStorage(youtubeDB, gYouTubeCache)
