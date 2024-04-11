8ae0bbdb5c76933b3f7679635b0d47cda4045c7c
Etai Levi <etai3000@gmail.com>
2024-04-11 21:26:49 +0300

    FIX: Removed env file and implement api key directly in service file

--- js/services/service.js
+++ js/services/service.js
@@ -1,11 +1,12 @@
 'use strict'
 
+const YOUTUBE_API_KEY = 'AIzaSyA13ULgHZv3MrvSQoFX_MR3C2-5ALY-wgQ'
 const youtubeDB = 'youtubeDB'
 const gYouTubeCache = loadFromStorage(youtubeDB) || {}
 
 function askVideo(value) {
     if (gYouTubeCache[value]) return Promise.resolve(gYouTubeCache[value])
-    return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${process.env.YOUTUBE_API_KEY}&q=${value}`)
+    return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${YOUTUBE_API_KEY}&q=${value}`)
         .then(res => { // resolve
             gYouTubeCache[value] = res.data.items
             saveToStorage(youtubeDB, gYouTubeCache)
