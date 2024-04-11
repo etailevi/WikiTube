e55e85afc255359fdb64a19fcf97b56e5b92f09d
Etai Levi <etai3000@gmail.com>
2024-04-11 18:33:32 +0300

    UPDATE: Created an env variable for the youtube api key

--- js/services/service.js
+++ js/services/service.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const API_KEY = ''
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY
 const youtubeDB = 'youtubeDB'
 const gYouTubeCache = loadFromStorage(youtubeDB) || {}
 
 function askVideo(value) {
     if (gYouTubeCache[value]) return Promise.resolve(gYouTubeCache[value])
-    return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${API_KEY}&q=${value}`)
+    return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${YOUTUBE_API_KEY}&q=${value}`)
         .then(res => { // resolve
             gYouTubeCache[value] = res.data.items
             saveToStorage(youtubeDB, gYouTubeCache)
